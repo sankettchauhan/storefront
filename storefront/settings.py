d97b1baf2bd91cf181e02a8fb8e2b0a2aac49dfa
sankettchauhan <sanket.chauhan4@gmail.com>
2022-01-23 02:47:32 +0530

    renamed store_custom to core

--- storefront/settings.py
+++ storefront/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
     'tags',
-    'likes'
+    'likes',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -143,6 +143,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK={
-    'COERCE_DECIMAL_TO_STRING':False,
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
 }
